9b788b0a0c4c46dae5984b21d73ff59b4d110263
loiclopez <loic.lopez@epitech.eu>
2018-01-22 17:04:17 +0100

    start project

---
+++ include/Thread/HttpHandler.hpp
@@ -0,0 +1,15 @@
+//
+// Created by loic lopez on 22/01/2018.
+//
+
+#ifndef CPP_ZIA_HANDLER_HXX
+#define CPP_ZIA_HANDLER_HXX
+
+
+class Handler
+{
+
+};
+
+
+#endif //CPP_ZIA_HANDLER_HXX
---
+++ sources/Thread/HttpHandler.cpp
@@ -0,0 +1,6 @@
+//
+// Created by loic lopez on 22/01/2018.
+//
+
+#include <Thread/HttpHandler.hpp>
+
