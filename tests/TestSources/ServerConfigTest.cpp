9a56ed415024a0ed7a82a76e56a3087b8427089a
loiclopez <loic.lopez@epitech.eu>
2018-02-06 21:56:50 +0100

    ServerConfig.cpp 90.4 % coverage

--- tests/TestSources/ServerConfigTest.cpp
+++ tests/TestSources/ServerConfigTest.cpp
@@ -5,6 +5,7 @@
 #define UNIT_TEST
 
 #include <gtest/gtest.h>
+#include <Core/ImplSocket.hpp>
 #include <Static/ServerConfig.hpp>
 
 TEST(ServerConfigTest, WebSiteConfs)
@@ -16,4 +17,19 @@ TEST(ServerConfigTest, isConfigFileExists)
 {
     ASSERT_FALSE(ServerConfig::isConfigFileExists("unittests"));
     ASSERT_TRUE(ServerConfig::isConfigFileExists("CMakeFiles"));
+}
+
+TEST(ServerConfigTest, LoadDefaultWebSiteConf)
+{
+    ServerConfig::LoadDefaultWebSiteConf();
+    ASSERT_TRUE(std::get<std::string>(ServerConfig::WebSiteConfs[0]["document_root"].v) == "html/");
+}
+
+TEST(ServerConfigTest, FormatIPAddress)
+{
+    std::shared_ptr<zia::api::ImplSocket> socket;
+    socket = std::shared_ptr<zia::api::ImplSocket>(new zia::api::ImplSocket(0));
+    struct sockaddr sockaddr1{};
+    sockaddr1 = ServerConfig::FormatIPAddress(socket->sockaddr, 80, "localhost");
+    ASSERT_TRUE(sizeof(sockaddr1) == sizeof(struct sockaddr));
 }
